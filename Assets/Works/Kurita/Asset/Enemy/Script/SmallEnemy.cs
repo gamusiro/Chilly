847cc82b7cd59e75af00a6b5efa00243839faf12
gamusiro <gamusiro310@gmail.com>
2024-01-16 09:43:28 +0900

    smallEnemyにぶつかったときのバグ解消

--- Assets/Works/Kurita/Asset/Enemy/Script/SmallEnemy.cs
+++ Assets/Works/Kurita/Asset/Enemy/Script/SmallEnemy.cs
@@ -17,9 +17,9 @@ public class SmallEnemy : Enemy
     private Vector3 _angle = Vector3.zero;
 
     //ƒJƒƒ‰
-    private LastCameraPhaseManager _cameraPhaseManager;
+    private GameCameraPhaseManager _cameraPhaseManager;
 
-    public void Initialize(LastCameraPhaseManager cpm, Transform parent,Transform player)
+    public void Initialize(GameCameraPhaseManager cpm, Transform parent,Transform player)
     {
         _cameraPhaseManager = cpm;
         _explosionParent = parent;
